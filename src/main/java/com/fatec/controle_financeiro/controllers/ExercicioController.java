3e1dc4545e37760c548a102eeb2508b8b45906ff
mi-yata <miyata@hotmail.com>
2024-08-29 22:25:01 -0300

    adicionado metodo post

--- src/main/java/com/fatec/controle_financeiro/controllers/ExercicioController.java
+++ src/main/java/com/fatec/controle_financeiro/controllers/ExercicioController.java
@@ -2,6 +2,8 @@ package com.fatec.controle_financeiro.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,25 @@ public class ExercicioController {
     public String HelloWorld(){
         return "Hello";
     }
+
+    @GetMapping("/reverter-nome/{nome}")
+    String reverterNome(@PathVariable String nome){
+        return new StringBuilder(nome).reverse().toString();
+    }
+
+    @PostMapping("/reverter-nome/")
+    String reverterNomeRequisicaoCorpo(@RequestBody String nome){
+        return new StringBuilder(nome).reverse().toString();
+    }
     
     @GetMapping("/contar-letras/{texto}")
     public String contaLetras(@PathVariable String texto){
+        int tamanho = texto.length();
+        return String.valueOf(tamanho);
+    }
+
+    @PostMapping("/contar-letras/")
+    String contaLetrasRequisicaoCorpo(@RequestBody String texto){
         int tamanho = texto.length();
         return String.valueOf(tamanho);
     }
